120c3d68f87f726654152f0371a996cd84b7112f
Will Curry <watercrafthd@gmail.com>
2016-09-16 11:30:43 +0100

    Add missing files

---
+++ src/TestCommand.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TestCommand implements Command {
+
+    private String name;
+
+    public TestCommand(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void execute() {
+        name = "newName";
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+}
